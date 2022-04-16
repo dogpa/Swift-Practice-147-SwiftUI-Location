f47ef7896faf6dc5e2c87e2058d6991a21760190
Dogpa <dogpa0130@gmail.com>
2022-04-16 21:29:50 +0800

    Initial Commit

---
+++ Swift Practice 147 SwiftUI Location/Swift_Practice_147_SwiftUI_LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_147_SwiftUI_LocationApp.swift
+//  Swift Practice 147 SwiftUI Location
+//
+//  Created by Dogpa's MBAir M1 on 2022/4/16.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_147_SwiftUI_LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
